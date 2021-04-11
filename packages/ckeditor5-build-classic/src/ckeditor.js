3d64bcb5fb8de8e3b1df5dc01375775c06339a21
Nikolay Sergeev <sergeev.n.s@gmail.com>
2021-04-11 21:26:44 +0300

    make it better

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -31,6 +31,8 @@ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';     // <--- ADDED
+
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
@@ -58,7 +60,8 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+        Alignment
 ];
 
 // Editor configuration.
@@ -67,6 +70,7 @@ ClassicEditor.defaultConfig = {
 		items: [
 			'heading',
 			'|',
+                        'alignment',
 			'bold',
 			'italic',
 			'link',
